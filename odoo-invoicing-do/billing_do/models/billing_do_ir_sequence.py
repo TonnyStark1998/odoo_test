a4ef48c46908518836806aa31820406176d7f1c0
Samuel Luciano <sa.lassis@gmail.com>
2020-09-20 19:15:41 -0400

    Change default directory for Billing Do App.

--- odoo-invoicing-do/billing_do/models/billing_do_ir_sequence.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, exceptions
-
-class BillingDoIrSequence(models.Model):
-    _inherit = "ir.sequence"
-
-    # Ir Sequence - New Fields
-    is_refund_sequence = fields.Boolean(default=False, store=True, tracking=True)
