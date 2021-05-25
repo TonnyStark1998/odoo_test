e37b8bc7a194da5d80b6d436dc4abce648969885
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2021-05-25 15:19:27 -0400

    Fix NCF & VAT validation for duplicity. Add required to code field on ir.sequence model.

--- odoo_invoicing_do/billing_do/models/billing_do_ir_sequence.py
+++ odoo_invoicing_do/billing_do/models/billing_do_ir_sequence.py
@@ -3,5 +3,8 @@ from odoo import models, fields, api, exceptions
 class BillingDoIrSequence(models.Model):
     _inherit = "ir.sequence"
 
+    # Account Move - Modified Fields
+    code = fields.Char(required=True)
+
     # Ir Sequence - New Fields
     is_refund_sequence = fields.Boolean(default=False, store=True, tracking=True)
