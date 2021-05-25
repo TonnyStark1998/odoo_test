f700424ad49d52647a14b250bec01308727b0d4e
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2021-05-25 09:40:52 -0400

    Fix misuse upper function on line 104 in billing_do_account_move.

--- odoo_invoicing_do/billing_do/models/billing_do_account_move.py
+++ odoo_invoicing_do/billing_do/models/billing_do_account_move.py
@@ -101,7 +101,7 @@ class BillingDoAccountMove(models.Model):
     @api.constrains('ncf', 'type', 'journal_id')
     def _check_ncf(self):
         for move in self:
-            if move.type in ['in_invoice', 'in_refund', 'in_receipt'] and self.is_tax_valuable and upper(self.journal_id.sequence_id.code) not in ['B11', 'B13']:
+            if move.type in ['in_invoice', 'in_refund', 'in_receipt'] and self.is_tax_valuable and self.journal_id.sequence_id.code.upper() not in ['B11', 'B13']:
                 try:
                     return self._validate_ncf(move.ncf)
                 except exceptions.ValidationError as ve:
