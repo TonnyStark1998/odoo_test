9ef78b311a5edeca05b0b3c4da47633ec16ec48b
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2021-07-12 15:08:28 -0400

    Add setUp() method for initialize data for tests cases.

--- odoo_invoicing_do/billing_do/tests/test_trn_helper.py
+++ odoo_invoicing_do/billing_do/tests/test_trn_helper.py
@@ -4,12 +4,14 @@ from odoo.tests import SingleTransactionCase, tagged
 @tagged('-standard')
 class TestTrnHelper(SingleTransactionCase):
 
+    def setUp(self):
+        self._invalid_trn = 'A0100114'
+
     def test_is_valid_trn_do(self):
-        _trn = 'A0100114'
         _trn_helper = self.env['billing.do.trn.helper']
         
         with self.assertRaises(exceptions.ValidationError) as ex:
-            _trn_helper.is_valid_trn_do(_trn)
+            _trn_helper.is_valid_trn_do(self._invalid_trn)
     
     def test_is_trn_from_journal_which_use_sequence(self):
         pass
