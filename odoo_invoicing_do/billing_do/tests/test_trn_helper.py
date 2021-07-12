6af8fba61184286c2f2bc891663215a6dba391fd
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2021-07-12 15:25:09 -0400

    Implement test method for journal use sequence.

--- odoo_invoicing_do/billing_do/tests/test_trn_helper.py
+++ odoo_invoicing_do/billing_do/tests/test_trn_helper.py
@@ -6,12 +6,12 @@ class TestTrnHelper(SingleTransactionCase):
 
     def setUp(self):
         self._invalid_trn = 'A0100114'
+        self._trn_helper = self.env['billing.do.trn.helper']
 
     def test_is_valid_trn_do(self):
-        _trn_helper = self.env['billing.do.trn.helper']
-        
         with self.assertRaises(exceptions.ValidationError) as ex:
-            _trn_helper.is_valid_trn_do(self._invalid_trn)
+            self._trn_helper.is_valid_trn_do(self._invalid_trn)
     
     def test_is_trn_from_journal_which_use_sequence(self):
-        pass
+        with self.assertRaises(exceptions.ValidationError) as ex:
+            self._trn_helper.is_trn_from_journal_which_use_sequence(self._trn_helper)
