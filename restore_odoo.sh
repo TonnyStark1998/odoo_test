cf5189ad67060f892f859879782ed04630315f38
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2022-11-11 23:49:37 -0400

    Update Infrastructure : Change value for data_dir in Odoo config file.

--- restore_odoo.sh
+++ restore_odoo.sh
@@ -3,8 +3,8 @@ docker compose --env-file .docker-compose-env up --force-recreate --detach
 
 DATABASE_USER=usr_odoo
 DATABASE_NAME=accounterprise
-ODOO_CONTAINER_ID=$(docker container ls -f name=accounterprise-odoo-ee-1 -q)
-DB_CONTAINER_ID=$(docker container ls -f name=accounterprise-odoo-db-1 -q)
+ODOO_CONTAINER_ID=$(docker container ls -f name=$DATABASE_NAME-odoo-ee-1 -q)
+DB_CONTAINER_ID=$(docker container ls -f name=$DATABASE_NAME-odoo-db-1 -q)
 BACKUP_FILE=$(ls -1t /var/data/production-backups/$DATABASE_NAME | head -1)
 PRODUCTION_SERVER_NAME=vmazlxol8odoofeprod01
 
@@ -27,7 +27,7 @@ docker container exec $DB_CONTAINER_ID psql -U usr_odoo -d postgres -c "CREATE D
 docker container exec $DB_CONTAINER_ID pg_restore --verbose --no-owner --no-privileges --clean --create --if-exists --port 5432 --format t --username $DATABASE_USER --role $DATABASE_USER --password --dbname $DATABASE_NAME /var/data/backups/$BACKUP_FILE
 
 # Copy Odoo files from production server
-scp -prvP 6546 samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo/* /var/data/$DATABASE_NAME/odoo
+rsync -vr -e "ssh -p 6546" samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo/filestore /var/data/$DATABASE_NAME/odoo/
 
 # Start Odoo container now that the database has been restored
 docker container start $ODOO_CONTAINER_ID
