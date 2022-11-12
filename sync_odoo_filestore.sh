7da223768438cd4d719a2d4f149f475c84652dc1
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2022-11-12 00:52:32 -0400

    Update Infrastructure : Fix some issues with mantainance scripts regarding variables.

--- sync_odoo_filestore.sh
+++ sync_odoo_filestore.sh
@@ -3,12 +3,12 @@ CURRENT_DATE=`date +%Y%m%d_%H%M`
 DATABASE_NAME=$1
 PRODUCTION_SERVER_NAME=vmazlxol8odoofeprod01
 
-if [[ -n ${DATABASE_NAME} ]]; then
+if [[ -z ${DATABASE_NAME} ]]; then
     echo "Must specify the database name."
     exit 1
 fi
 
-if [[ $DATABASE_NAME -eq "accounterprise" ]]; then
+if [[ "$DATABASE_NAME" == "accounterprise" ]]; then
     rsync -vr -e "ssh -p 6546" samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo/filestore /var/data/$DATABASE_NAME/odoo/
 else
     rsync -vr -e "ssh -p 6546" samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo-$DATABASE_NAME/filestore /var/data/$DATABASE_NAME/odoo/
