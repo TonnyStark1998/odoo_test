3fca04c8303a65bf4d3ee0e615f2b213eb724a8b
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2022-11-17 22:39:44 -0400

    Update Infrastructure : Fix restore_database shell script and add backup_database shell script.

--- restore_odoo.sh
+++ restore_odoo.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/sh
 DATABASE_USER=usr_odoo
 DATABASE_NAME=$1
 
@@ -6,6 +6,10 @@ if [[ -z ${DATABASE_NAME} ]]; then
     echo "Must specify the database name."
     exit -1
 fi
+
+# Get up and running the services required for this app
+cd /opt/odoo/$DATABASE_NAME
+docker compose --env-file .docker-compose-env up --force-recreate --detach
 
 ODOO_CONTAINER_ID=$(docker container ls -f name=$DATABASE_NAME-odoo-ee-1 -q)
 DB_CONTAINER_ID=$(docker container ls -f name=$DATABASE_NAME-odoo-db-1 -q)
@@ -21,9 +25,6 @@ if [[ -z ${DB_CONTAINER_ID} ]]; then
     exit -1
 fi
 
-# Get up and running the services required for this app
-docker compose --env-file .docker-compose-env up --force-recreate --detach
-
 # Stop Odoo container until database is restored
 docker container stop $ODOO_CONTAINER_ID
 
@@ -38,14 +39,7 @@ docker container exec $DB_CONTAINER_ID psql -U $DATABASE_USER -d postgres -c "DR
 docker container exec $DB_CONTAINER_ID psql -U $DATABASE_USER -d postgres -c "CREATE DATABASE $DATABASE_NAME WITH TEMPLATE template0 OWNER $DATABASE_USER;"
 
 # Restore last database backup
-docker container exec $DB_CONTAINER_ID pg_restore --verbose --no-owner --no-privileges --clean --create --if-exists --port 5432 --format t --username $DATABASE_USER --role $DATABASE_USER --password --dbname $DATABASE_NAME /var/data/backups/$BACKUP_FILE
-
-# Copy Odoo files from production server
-if [[ "$DATABASE_NAME" == "accounterprise"]]; then
-    rsync -vr -e "ssh -p 6546" samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo/filestore /var/data/$DATABASE_NAME/odoo/
-else
-    rsync -vr -e "ssh -p 6546" samuel.luciano@$PRODUCTION_SERVER_NAME:/var/data/odoo-$DATABASE_NAME/filestore /var/data/$DATABASE_NAME/odoo/
-fi
+docker container exec $DB_CONTAINER_ID pg_restore --verbose --no-owner --no-privileges --clean --create --if-exists --port 5432 --format c --username $DATABASE_USER --role $DATABASE_USER --password --dbname $DATABASE_NAME /var/data/backups/$BACKUP_FILE
 
 # Start Odoo container now that the database has been restored
 docker container start $ODOO_CONTAINER_ID
