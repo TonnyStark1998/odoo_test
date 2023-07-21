4cf1b44cce838ea2dd5c3c94ad3ba27fc2b75a98
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2023-07-21 08:49:49 -0300

    Update Odoo 16: Changed the way the subtotal & total are calculated based on the coverage.

--- third_party_addons/common/ars_do/models/ars_do_account_move_line.py
+++ third_party_addons/common/ars_do/models/ars_do_account_move_line.py
@@ -20,8 +20,7 @@ class ArsDoAccountMoveLine(models.Model):
                 if not line.move_id.is_invoice(include_receipts=True):
                     continue
 
-                line.update(line._get_price_total_and_subtotal())
-                line.update(line._get_fields_onchange_subtotal())
+                line._compute_totals()
             elif line.move_id.healthcare_invoice == 'not_healthcare_invoice':
                 if line.coverage > 0.0:
                     line.coverage = 0.0
@@ -32,28 +31,56 @@ class ArsDoAccountMoveLine(models.Model):
                         }
                     }
     
-    def _get_price_total_and_subtotal(self, 
-                                        price_unit=None, 
-                                        quantity=None, 
-                                        discount=None, 
-                                        currency=None, 
-                                        product=None, 
-                                        partner=None, 
-                                        taxes=None, 
-                                        move_type=None):
-        self.ensure_one()
-        price_total_and_subtotal = super(ArsDoAccountMoveLine, self)._get_price_total_and_subtotal(price_unit, 
-                                                                                                    quantity, 
-                                                                                                    discount, 
-                                                                                                    currency, 
-                                                                                                    product, 
-                                                                                                    partner, 
-                                                                                                    taxes, 
-                                                                                                    move_type)
-        if self.coverage > 0:
-            price_total_and_subtotal['price_subtotal'] = \
-                price_total_and_subtotal['price_subtotal'] * (1 - (self.coverage / 100))
+    @api.depends('coverage')
+    def _compute_totals(self):
+        for line in self:
+            if line.display_type != 'product':
+                line.price_total = line.price_subtotal = False
+            # Compute 'price_subtotal'.
+            line_discount_price_unit = line.price_unit * (1 - (line.discount / 100.0))
 
-        price_total_and_subtotal['coverage'] = self.coverage
+            if line.move_id.healthcare_invoice == 'healthcare_invoice':
+                line_discount_price_unit = line_discount_price_unit * (1 - (line.coverage / 100.0))
 
-        return price_total_and_subtotal
+            subtotal = line.quantity * line_discount_price_unit
+
+            # Compute 'price_total'.
+            if line.tax_ids:
+                taxes_res = line.tax_ids.compute_all(
+                    line_discount_price_unit,
+                    quantity=line.quantity,
+                    currency=line.currency_id,
+                    product=line.product_id,
+                    partner=line.partner_id,
+                    is_refund=line.is_refund,
+                )
+                line.price_subtotal = taxes_res['total_excluded']
+                line.price_total = taxes_res['total_included']
+            else:
+                line.price_total = line.price_subtotal = subtotal
+
+    # def _get_price_total_and_subtotal(self, 
+    #                                     price_unit=None, 
+    #                                     quantity=None, 
+    #                                     discount=None, 
+    #                                     currency=None, 
+    #                                     product=None, 
+    #                                     partner=None, 
+    #                                     taxes=None, 
+    #                                     move_type=None):
+    #     self.ensure_one()
+    #     price_total_and_subtotal = super(ArsDoAccountMoveLine, self)._get_price_total_and_subtotal(price_unit, 
+    #                                                                                                 quantity, 
+    #                                                                                                 discount, 
+    #                                                                                                 currency, 
+    #                                                                                                 product, 
+    #                                                                                                 partner, 
+    #                                                                                                 taxes, 
+    #                                                                                                 move_type)
+    #     if self.coverage > 0:
+    #         price_total_and_subtotal['price_subtotal'] = \
+    #             price_total_and_subtotal['price_subtotal'] * (1 - (self.coverage / 100))
+
+    #     price_total_and_subtotal['coverage'] = self.coverage
+
+    #     return price_total_and_subtotal
