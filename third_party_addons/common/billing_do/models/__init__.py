9ae555fb657d8997a8ab6c500ba537b8d9438e31
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2023-11-13 16:55:32 -0400

    Bug 268: Added new security group to control if attachment is required or not in MRP productions.

--- third_party_addons/common/billing_do/models/__init__.py
+++ third_party_addons/common/billing_do/models/__init__.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from . import billing_do_exceptions,\
-				billing_do_res_config_settings,\
-				billing_do_trn_type,\
-				billing_do_vat_helper,\
-				billing_do_trn_helper,\
-				billing_do_http_service_helper, \
-				billing_do_authentication_http_service_helper, \
-				billing_do_citizen_http_service_helper, \
-				billing_do_vat_http_service_helper, \
-				billing_do_trn_http_service_helper,\
-				billing_do_account_journal, \
-				billing_do_account_move,\
-				billing_do_ir_sequence, \
-				billing_do_res_partner,\
-				billing_do_res_company,\
-				billing_do_account_tax, \
-				billing_do_account_account, \
-				billing_do_account_payment, \
-				billing_do_product_product, \
-				billing_do_stock_warehouse,\
-				billing_do_sale_report, \
-				billing_do_tax_report, \
-				billing_do_tax_report_item_common,\
-				billing_do_tax_report_item_606, \
-				billing_do_tax_report_item_607,\
-				billing_do_res_currency,\
-				billing_do_currency_rate_http_service_helper,\
-				billing_do_account_move_line,\
-				billing_do_account_payment_register,\
+                billing_do_res_config_settings,\
+                billing_do_trn_type,\
+                billing_do_vat_helper,\
+                billing_do_trn_helper,\
+                billing_do_http_service_helper, \
+                billing_do_authentication_http_service_helper, \
+                billing_do_citizen_http_service_helper, \
+                billing_do_vat_http_service_helper, \
+                billing_do_trn_http_service_helper,\
+                billing_do_account_journal, \
+                billing_do_account_move,\
+                billing_do_ir_sequence, \
+                billing_do_res_partner,\
+                billing_do_res_company,\
+                billing_do_account_tax, \
+                billing_do_account_account, \
+                billing_do_account_payment, \
+                billing_do_product_product, \
+                billing_do_stock_warehouse,\
+                billing_do_sale_report, \
+                billing_do_tax_report, \
+                billing_do_tax_report_item_common,\
+                billing_do_tax_report_item_606, \
+                billing_do_tax_report_item_607,\
+                billing_do_res_currency,\
+                billing_do_currency_rate_http_service_helper,\
+                billing_do_account_move_line,\
+                billing_do_account_payment_register,\
                 billing_do_ncf_type,\
-                billing_do_account_move_reversal
+                billing_do_account_move_reversal,\
+                billing_do_mrp_production
