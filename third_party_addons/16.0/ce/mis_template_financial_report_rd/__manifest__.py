cb81b49a3915c3ca827939290fdf5b7a1ea7c15c
Carlos Lopez <celm1990@gmail.com>
2024-06-18 22:59:03 -0500

    AB#283 AB#304 mis_template_financial_report_rd: data para reporte de Perdidas y Ganancias

---
+++ third_party_addons/16.0/ce/mis_template_financial_report_rd/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Plantilla de reportes financieros para RD",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Carlos Lopez",
+    "website": "https://github.com/salucianol/odoo-accounterprise-rd",
+    "category": "Localization",
+    "depends": ["mis_builder"],
+    "data": [
+        "data/mis_report_style.xml",
+        "data/mis_report.xml",
+        "data/mis_report_kpi_pyg_data.xml",
+    ],
+}
