f3309ca30e8a99a4163d5ef208ac2e0d50b41d1e
Carlos Lopez <celm1990@gmail.com>
2024-06-25 15:52:17 -0500

    AB#311 balance sheet - add new options into 'account_type' field. starts with "equity_" to widget taken correctly

--- third_party_addons/16.0/common/billing_do/models/billing_do_account_account.py
+++ third_party_addons/16.0/common/billing_do/models/billing_do_account_account.py
@@ -11,3 +11,17 @@ class BillingDoAccountAccount(models.Model):
         ('RET-ISR-606', 'Retencion ISR 606'),
         ('RET-ISR-607', 'Retencion ISR 607'),
     ], string='Withholding Tax Type', required=False, store=True, copy=True)
+    account_type = fields.Selection(
+        selection_add=[
+            ('equity_capital_social', 'Capital Social'),
+            ('equity_accion_tesoreria', 'Acciones en Tesoreria'),
+            ('equity_reserva_legal', 'Reserva Legal'),
+            ('equity_superavit_revaluacion', 'Superávit por revaluación'),
+        ],
+        ondelete={
+            'equity_capital_social': 'set equity', 
+            'equity_accion_tesoreria': 'set equity', 
+            'equity_reserva_legal': 'set equity', 
+            'equity_superavit_revaluacion': 'set equity'
+        },
+    )
